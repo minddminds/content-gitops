4a1b3fca659f1157d23dfeb67c29ee3f6372a736
Ade Johnson <a_jaws1408@hotmail.com>
2023-04-20 20:36:26 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Ade Students!"
+    return "Hello Ades Student!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
